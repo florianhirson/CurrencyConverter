eae8945a5ab355f34d3effd5bfd23af956e6bc49
Florian Hirson <florian.hirson@etudiant.univ-lille1.fr>
2017-06-28 21:56:33 +0200

    Correction de l'erreur de reseau en premier plan, gestion des permissions

--- app/src/main/java/fr/hirsonf/florian/currencyconverter/MainActivity.java
+++ app/src/main/java/fr/hirsonf/florian/currencyconverter/MainActivity.java
@@ -1,17 +1,215 @@
 package fr.hirsonf.florian.currencyconverter;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.http.RequestQueue;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private Spinner from, to;
+    private Button swap;
+    private EditText saisie;
+    private TextView result;
+    private String currencyFrom, currencyTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.INTERNET},
+                1);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
+                1);
+
+
+        from = (Spinner) findViewById(R.id.from);
+        to = (Spinner) findViewById(R.id.to);
+        swap = (Button) findViewById(R.id.swap);
+        saisie = (EditText) findViewById(R.id.editText);
+        result = (TextView) findViewById(R.id.result);
+
+
+        swap.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                swap(from, to);
+            }
+        });
+
+        saisie.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                currencyFrom = from.getSelectedItem().toString().substring(0, 3);
+                currencyTo = to.getSelectedItem().toString().substring(0, 3);
+
+                if (!isConnected()) {
+                    Toast.makeText(getApplicationContext(), "Aucune connexion à internet.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                new FetchTask().execute("http://quote.yahoo.com/d/quotes.csv?s=" + currencyFrom + currencyTo + "=X&f=l1&e=.csv");
+                /*
+                try {
+                    result.setText(convert(Integer.parseInt(saisie.getText().toString()),getTaux(currencyFrom,currencyTo),currencyFrom,currencyTo));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                */
+            }
+        });
+
+        from.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currencyFrom = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        to.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currencyTo = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
+
+    private boolean isConnected() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    public void swap(Spinner from, Spinner to) {
+        String tmp = from.getSelectedItem().toString();
+        from.setSelection(((ArrayAdapter<String>)to.getAdapter()).getPosition(to.getSelectedItem().toString()));
+        to.setSelection(((ArrayAdapter<String>)to.getAdapter()).getPosition(tmp));
+    }
+
+
+    public String convert (Integer montant, float taux, String currencyFrom, String currencyTo) {
+        return montant +" "+ currencyFrom+ " = " + montant*taux+ " " + currencyTo;
+    }
+
+    private class FetchTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            InputStream inputStream = null;
+            HttpURLConnection conn = null;
+
+            String stringUrl = strings[0];
+            try {
+                URL url = new URL(stringUrl);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.connect();
+                int response = conn.getResponseCode();
+                if (response != 200) {
+                    return null;
+                }
+
+                inputStream = conn.getInputStream();
+                if (inputStream == null) {
+                    return null;
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                StringBuffer buffer = new StringBuffer();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                    buffer.append("\n");
+                }
+
+                return new String(buffer);
+            } catch (IOException e) {
+                return null;
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException ignored) {
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (s == null) {
+                Toast.makeText(getApplicationContext(), "Aucune connexion à internet.", Toast.LENGTH_SHORT).show();
+            } else {
+                result.setText(convert(Integer.parseInt(saisie.getText().toString()),Float.parseFloat(s),currencyFrom,currencyTo));
+            }
+
+        }
+    }
+
+
 }
